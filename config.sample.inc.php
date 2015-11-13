19c073cdc0143b5e0f4c85d164b9a4fcf85dd09f
Manuel Zavatta <manuel.zavatta@gmail.com>
2015-11-13 16:35:40 +0100

    Sample configuration

---
+++ config.sample.inc.php
@@ -0,0 +1,21 @@
+<?php
+
+ // debug
+ $debug=TRUE;
+ 
+ // path and dir
+ $config->path="/var/www";
+ $config->dir="/thermostat/";
+ 
+ // template
+ $config->template="default";
+
+ // database parameters
+ $config->db_type="mysql";
+ $config->db_host="localhost";
+ $config->db_port="3306";
+ $config->db_name="thermostat";
+ $config->db_user="root";
+ $config->db_pass="root";
+
+?>
