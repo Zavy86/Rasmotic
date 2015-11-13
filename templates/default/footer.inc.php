3f658f2e3ab5fd62fa5dea4ecff961f4394b1316
Manuel Zavatta <manuel.zavatta@gmail.com>
2015-11-13 16:36:32 +0100

    Defaut template

---
+++ templates/default/footer.inc.php
@@ -0,0 +1,22 @@
+<?php
+
+ // show debug logs
+ if($debug){
+  echo "<br><br><div id='debug'>\n <pre>\n";
+  foreach($_SESSION['log'] as $log){echo "<code class='".$log[0]."'>".$log[1]."</code>\n";}
+  echo "  </pre>\n</div>\n";
+ }
+
+?>
+
+  </div> <!-- /container -->
+    
+  <script type="text/javascript">
+   // update timestamp every 10 sec
+   setInterval(function(){
+    $("#nav_datetime").load("now.inc.php");
+   },50000);
+  </script>
+    
+ </body>
+</html>
---
+++ templates/default/header.inc.php
@@ -0,0 +1,82 @@
+<!DOCTYPE html>
+<html lang="en">
+ <head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="description" content="">
+  <meta name="author" content="">
+  <link rel="icon" href="<?php echo TEMPLATE; ?>images/favicon.ico">
+
+  <title>Rasmostato - Raspbery Thermostat</title>
+
+  <!-- jQuery -->
+  <script src="<?php echo TEMPLATE; ?>js/jquery-1.11.3.min.js"></script>
+
+  <!-- Bootstrap -->
+  <link href="<?php echo TEMPLATE; ?>css/bootstrap-3.3.5.min.css" rel="stylesheet">
+  <script src="<?php echo TEMPLATE; ?>js/bootstrap-3.3.5.min.js"></script>
+
+  <!-- Bootstrap Toggle -->
+  <script src="<?php echo TEMPLATE; ?>js/bootstrap-toggle-2.2.0.min.js"></script>
+  <link href="<?php echo TEMPLATE; ?>css/bootstrap-toggle-2.2.0.min.css" rel="stylesheet">
+  
+  <!-- CSS -->
+  <link href="<?php echo TEMPLATE; ?>css/style.css" rel="stylesheet">
+
+  <!-- FIX per browser fuori standard -->
+  <script src="<?php echo TEMPLATE; ?>js/ie10-viewport-bug-workaround.js"></script>
+  <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+  <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+  <![endif]-->
+ </head>
+ 
+ <body>
+  
+  <!-- navbar -->
+  <nav class="navbar navbar-default navbar-static-top">
+   <div class="container">
+    <div class="navbar-header">
+
+     <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+      <span class="sr-only">Toggle navigation</span>
+      <span class="icon-bar"></span>
+      <span class="icon-bar"></span>
+      <span class="icon-bar"></span>
+     </button>
+     
+     <a class="navbar-brand" href="#"><img alt="Brand" src="<?php echo TEMPLATE; ?>images/brand.png" width="20"></a>
+     <a class="navbar-brand" href="index.php">Rasmostato</a>
+     
+    </div><!--/navbar-header -->
+    
+    <div id="navbar" class="navbar-collapse collapse">
+     <ul class="nav navbar-nav">
+      <li<?php if(VIEW=="overview"){echo " class='active'";} ?>><a href="index.php?view=overview">Overview</a></li>
+      <li<?php if(VIEW=="planner"){echo " class='active'";} ?>><a href="index.php?view=planner">Planner</a></li>
+      <li<?php if(VIEW=="settings"){echo " class='active'";} ?>><a href="index.php?view=settings">Settings</a></li>
+      <li class="dropdown">
+       <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Altro <span class="caret"></span></a>
+       <ul class="dropdown-menu">
+        <li><a href="#">Action</a></li>
+        <li><a href="#">Another action</a></li>
+        <li><a href="#">Something else here</a></li>
+        <li role="separator" class="divider"></li>
+        <li class="dropdown-header">Nav header</li>
+        <li><a href="#">Separated link</a></li>
+        <li><a href="#">One more separated link</a></li>
+       </ul>
+      </li>
+     </ul>
+     <ul class="nav navbar-nav navbar-right">
+      <li><a href="#" id="nav_datetime"><?php include("now.inc.php"); ?></a></li>
+     </ul>
+    </div><!--/navbar-collapse -->
+
+   </div><!--/container -->
+  </nav>
+
+  <!-- container -->
+  <div class="container">
