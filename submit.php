7c066f6c98ce91035bde69ca5854e1c8897d9213
Manuel Zavatta <manuel.zavatta@gmail.com>
2015-11-13 16:42:50 +0100

    Submit ajax and standard

---
+++ submit.php
@@ -0,0 +1,76 @@
+<?php 
+
+ // include api
+ require_once("api.inc.php");
+
+ // acquire variables
+ $g_act=$_REQUEST['act'];
+ 
+ //
+ switch($g_act){
+  // standard functions
+  case "settings_save":settings_save();break;
+  
+  // ajax functions
+  case "modality_toggle":modality_toggle();break;
+  case "manual_temperature":manual_temperature();break;
+  
+  // default
+  default:exit(header("location: index.php?alert=submitActionNotFound&action=".$g_act));
+ }
+ 
+ 
+ // settings save
+ function settings_save(){
+  // acquire variables
+  $p_manual_timeout=$_REQUEST["manual_timeout"];
+  // checks
+  if(!is_numeric($p_manual_timeout)){
+   $_SESSION['log'][]=array("error","UPGRADE manual_timeout numeric value needed");
+   return false;
+  }
+  // update manual toggle
+  api_setting_update("manual_timeout",$p_manual_timeout);
+  // redirect
+  exit(header("location: index.php?view=settings"));
+ }
+ 
+ 
+ 
+ 
+ 
+ 
+ // update modality
+ function modality_toggle(){
+  // acquire variables
+  $p_manual_toggle=$_REQUEST["manual_toggle"];
+  // checks on converts
+  if($p_manual_toggle=="true"){$p_manual_toggle="manual";}else{$p_manual_toggle="auto";}
+  // update manual toggle
+  api_setting_update("modality",$p_manual_toggle);
+  // if is manual update started datetime
+  if($p_manual_toggle=="manual"){api_setting_update("manual_started",api_datetime_now());}
+   else{api_setting_update("manual_started",NULL);}
+ }
+ 
+ // update manual temperature
+ function manual_temperature(){
+  // acquire variables
+  echo $p_temperature=$_REQUEST["temperature"];
+  // checks
+  if(!is_numeric($p_temperature)){
+   $_SESSION['log'][]=array("error","UPGRADE manual_temperature numeric value needed");
+   return false;
+  }
+  // update manual temperature
+  api_setting_update("manual_temperature",number_format($p_temperature,1,".",","));
+ }
+ 
+ // 
+ if($_REQUEST['debug']){
+  echo "<br><br><div id='debug'>\n <pre>\n";
+  foreach($_SESSION['log'] as $log){echo "<code class='".$log[0]."'>".$log[1]."</code>\n";}
+  echo "  </pre>\n</div>\n";
+ }
+ 
+?>
